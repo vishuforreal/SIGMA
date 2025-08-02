1d03ce3e7250cf1d85c9caf83acf28c29c67cc6f
vishuforreal <vishuuforreal@gmail.com>
2025-08-02 14:15:08 +0530

    upload

---
+++ DELTA/DAY_15/app.js
@@ -0,0 +1,61 @@
+// let n = prompt("Enter quarter:");
+// switch (n) {
+//     case "1":
+//         alert("January, February, March");
+//         break;
+//     case "2":
+//         alert("April, May, June");
+//         break;
+//     case "3":
+//         alert("July, August, September");
+//         break;
+//     case "4":
+//         alert("October, November, December");
+//         break;
+//     default:
+//         alert("Invalid entry");
+//         break;
+// }
+
+
+// let gs = prompt("Enter your words");
+
+// if (gs[0] == "a" || gs[0] == "A") {
+//     if (gs.length>5) {
+//         alert("Golden String");
+//     }
+// }
+// else {
+//     alert("Bad String");
+// }
+
+// let a = prompt("Enter 1st number :");
+// let b = prompt("Enter 2nd number :");
+// let c = prompt("Enter 3rd number :");
+
+// if (a>b && a>c) {
+//     alert("A is greater.");
+// }
+
+// else if (b>c && b>a) {
+//     alert("B is greater.");
+// }
+
+// else if (c>a && c>b){
+//     alert("C is greater.");
+// }
+
+// else {
+//     alert("Invalid input!");
+// }
+
+
+// let a = prompt("Enter 1st number:");
+// let b = prompt("Enter 2nd number:");
+
+// if (a%10===b%10) {
+//     alert("Last digit are same.");
+// }
+// else {
+//     alert("Last digit are not same.");
+// }
