1d47c5d58ccc373b68dddf39465efd830a5a2f50
vishuforreal <vishuuforreal@gmail.com>
2025-08-05 10:58:42 +0530

    add all

---
+++ DELTA/DAY_18/app.js
@@ -0,0 +1,52 @@
+// const n = prompt("Enter your maximum range:");
+// let a = Math.floor((Math.random() * n) + 1);
+// let guess;
+// let i = 1;
+// console.log('');
+// console.log('');
+// console.log('');
+// console.log('');
+// console.log('');
+// console.log('');
+// console.log('');
+// console.log('');
+// console.log('');
+// while (i <= 5) {
+//     guess = prompt("Enter your guess:");
+//     if (guess == a) {
+//         break;
+//     }
+//     else if (guess > a) {
+//         console.log("Your guess is greater than exact number");
+//         console.log(`${5 - i} chance left`);
+//     }
+//     else if (guess < a) {
+//         console.log("Your guess is less than exact number");
+//         console.log(`${5 - i} chance left`);
+//     }
+//     i++;
+// }
+// if (i <= 5 && guess == a) {
+//     console.log("Number guessed successful, You won!");
+// }
+// else {
+//     console.log("You lose!");
+// }
+
+// let n = Math.floor((Math.random() * 6) + 1);
+// console.log(n);
+
+// let car = {
+//     name: "Maruti",
+//     model: "Dzire",
+//     color: "Red"
+// };
+
+// console.log(car["color"]);
+
+// let person ={
+//     name: "Vishu",
+//     age: 20,
+//     city: "patna"
+// };
+
